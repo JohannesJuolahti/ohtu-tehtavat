4d7a93a94afb131274cb0554917aba75694fcc2a
Johannes J <johannes.v.juolahti@utu.fi>
2021-03-06 17:11:46 +0200

    Base for week 2.

---
+++ src/main/java/viikko2/AppConfig.java
@@ -0,0 +1,8 @@
+package viikko2;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "ohtu.verkkokauppa")
+public class AppConfig  {}

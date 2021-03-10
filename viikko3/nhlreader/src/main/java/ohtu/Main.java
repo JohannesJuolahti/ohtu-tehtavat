b03ac0a094b27cbd37752a86f62fd08fe0253066
Johannes J <johannes.v.juolahti@utu.fi>
2021-03-10 16:01:06 +0200

    Viikko 3. Tehtävät 1-3.

--- viikko3/nhlreader/src/main/java/ohtu/Main.java
+++ viikko3/nhlreader/src/main/java/ohtu/Main.java
@@ -1,14 +1,42 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ohtu;
 
-/**
- *
- * @author mluukkai
- */
+import com.google.gson.Gson;
+
+import java.io.IOException;
+
+import org.apache.http.client.fluent.Request;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
 public class Main {
-    
+    public static void main(String[] args) throws IOException {
+        String url = "https://nhlstatisticsforohtu.herokuapp.com/players";
+
+        String bodyText = Request.Get(url).execute().returnContent().asString();
+
+        //System.out.println("json-muotoinen data:");
+        //System.out.println(bodyText);
+
+        Gson mapper = new Gson();
+        Player[] players = mapper.fromJson(bodyText, Player[].class);
+
+        ArrayList<Player> finnishPlayers = new ArrayList<>();
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm:ss dd/MM/yyyy");
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println("Players from FIN " + dtf.format(now));
+        for (Player player : players) {
+            if (player.getNationality().contentEquals("FIN")) {
+                finnishPlayers.add(player);
+            }
+        }
+        Collections.sort(finnishPlayers, Player::compareTo);
+        for (Player p : finnishPlayers) {
+            System.out.println(p.toString());
+        }
+    }
+
 }
--- viikko3/nhlreader/src/main/java/ohtu/Player.java
+++ viikko3/nhlreader/src/main/java/ohtu/Player.java
@@ -1,8 +1,12 @@
 
 package ohtu;
 
-public class Player {
+public class Player implements Comparable<Player> {
     private String name;
+    private String nationality;
+    private String team;
+    private int assists;
+    private int goals;
 
     public void setName(String name) {
         this.name = name;
@@ -12,9 +16,46 @@ public class Player {
         return name;
     }
 
+    public String getNationality() {
+        return nationality;
+    }
+
+    public int getAssists() {
+        return assists;
+    }
+
+    public int getGoals() {
+        return goals;
+    }
+
+    public int getTotalScore() {
+        return getAssists() + getGoals();
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public String getAmountOfSpaces(int prettySpaces) {
+        StringBuilder spaces = new StringBuilder();
+        for (int i = name.length(); i <= prettySpaces; i++) {
+            spaces.append(" ");
+        }
+        return spaces.toString();
+    }
+
     @Override
     public String toString() {
-        return name;
+        return this.getName() + this.getAmountOfSpaces(20) +
+                this.getTeam() + "   " + this.getGoals() + this.getAmountOfSpaces(2)
+                + " + " + this.getAmountOfSpaces(2) +
+                this.getAssists() + " = " + this.getTotalScore();
     }
-      
+
+
+    @Override
+    public int compareTo(Player p1) {
+        return p1.getTotalScore() - this.getTotalScore();
+    }
+
 }

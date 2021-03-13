bb73e15b26eca183e8a81dbce45742833b228525
Johannes J <johannes.v.juolahti@utu.fi>
2021-03-13 13:08:49 +0200

    Viikko 3. Tehtävä 8.

--- viikko3/WebLogin/src/main/java/ohtu/Tester.java
+++ viikko3/WebLogin/src/main/java/ohtu/Tester.java
@@ -9,33 +9,64 @@ public class Tester {
 
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
-
         driver.get("http://localhost:4567");
-        
+        createUser(driver, "onnistunut", "luonti12");
+        driver.get("http://localhost:4567");
+        failedLogin(driver);
+        driver.get("http://localhost:4567");
+        createUserAndLogout(driver);
+        driver.quit();
+
+    }
+
+    private static void sleep(int n) {
+        try {
+            Thread.sleep(n * 1000);
+        } catch (Exception e) {
+        }
+    }
+
+    private static void createUser(WebDriver driver, String username, String password) {
+        // Uuden käyttäjätunnuksen luominen
         sleep(2);
-        
-        WebElement element = driver.findElement(By.linkText("login"));
+        WebElement element = driver.findElement(By.linkText("register new user"));
         element.click();
-
         sleep(2);
-
         element = driver.findElement(By.name("username"));
-        element.sendKeys("pekka");
+        element.sendKeys(username);
         element = driver.findElement(By.name("password"));
-        element.sendKeys("akkep");
-        element = driver.findElement(By.name("login"));
-        
+        element.sendKeys(password);
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys(password);
+        element = driver.findElement(By.name("signup"));
         sleep(2);
         element.submit();
-
         sleep(3);
-        
-        driver.quit();
     }
-    
-    private static void sleep(int n){
-        try{
-            Thread.sleep(n*1000);
-        } catch(Exception e){}
+
+    private static void failedLogin(WebDriver driver) {
+        // Epäonnistunut kirjautuminen: oikea käyttäjätunnus, väärä salasana
+        sleep(2);
+        WebElement element = driver.findElement(By.linkText("login"));
+        element.click();
+        sleep(2);
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("onnistunut");
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("luonti11");
+        element = driver.findElement(By.name("login"));
+        sleep(2);
+        element.submit();
+        sleep(3);
+    }
+
+    private static void createUserAndLogout(WebDriver driver) {
+        // Uuden käyttäjätunnuksen luomisen jälkeen tapahtuva ulkoskirjautuminen sovelluksesta
+        createUser(driver, "onnistunyt", "luonti13");
+        WebElement element = driver.findElement(By.linkText("continue to application mainpage"));
+        element.click();
+        sleep(2);
+        element = driver.findElement(By.linkText("logout"));
+        element.click();
     }
 }
